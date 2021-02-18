e973472e31eefb541f224aa9dafd23c68c0f0ca5
Snehal Dhameliya <snehaldhameliya17@gmail.com>
2021-02-18 14:19:28 +0530

    Changed Main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Chat UI',
-      debugShowCheckedModeBanner: false,
+      title: 'Flutter Chat UI Changed',
+      debugShowCheckedModeBanner: true,
       theme: ThemeData(
         fontFamily: 'OpenSans',
         primaryColor: Colors.red,
